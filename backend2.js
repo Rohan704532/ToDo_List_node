5703a858fdeaa2e47c6b5679e28e737238886255
Rohan Singh <rohan704532@gmail.com>
2023-07-17 18:14:27 +0530

    First_Commit

---
+++ backend2.js
@@ -0,0 +1,95 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser")
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json())
+const cors = require("cors");
+app.use(cors());
+
+const USERNAME=process.env.DB_USERNAME
+const PASSWORD=process.env.DB_PASSWORD
+
+DB = `mongodb+srv://${USERNAME}:${PASSWORD}@todo-list-node.ry2mpau.mongodb.net/`
+mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log("connected with mongodb")
+}).catch((err) => {
+    console.log(err)
+})
+
+const todoSchema = mongoose.Schema({
+    title: String
+})
+
+const list = new mongoose.model("List", todoSchema)
+
+app.post('/add', async (req, res) => {
+    const { title } = req.body;
+    //console.log(title)
+    const List = await list.create(req.body)
+    res.status(200).json({
+        success: true,
+        List
+    })
+})
+
+app.post('/addp', async (req, res) => {
+    const { title } = req.body;
+    //console.log(title)
+    const List = await list.updateOne({$push:{$each:title,$position:0}})
+    res.status(200).json({
+        success: true,
+        List
+    })
+})
+
+
+app.get('/get', async (req, res) => {
+    const todos = await list.find();
+    res.status(200).json({ success: true, todos })
+})
+
+app.delete('/deleteId', async (req, res) => {
+    todo = await list.find({ title: req.body.title })
+    if (todo.length == 0) {
+        return res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    } else {
+        await list.deleteMany({ title: req.body.title })
+        return res.status(200).json({
+            success: true,
+            message: "Product is deleted successfully"
+        })
+    }
+
+})
+
+app.delete('/deleteAll', async (req, res) => {
+    const todos = await list.find();
+    await list.deleteMany()
+
+    if (todos.length == 0) {
+        console.log("empty")
+        return res.status(400).json({
+            success: false,
+            message: "Is Empty"
+        })
+    } else {
+        res.status(200).json({
+            success: true,
+            message: "deleted all"
+        })
+    }
+})
+
+dotenv.config({path:'./config.env'})
+const PORT = process.env.PORT || 3000
+
+app.use(express.static("build"))
+
+app.listen(PORT, () => {
+    console.log("server is  working on 3000")
+})
---
+++ build/Assignment_01.js
@@ -0,0 +1,282 @@
+/*const input = document.querySelector(".input");
+const btna = document.querySelector(".Add");
+const btnp = document.querySelector(".Pri");
+const btnd = document.querySelector(".Del")
+const ul = document.querySelector(".ul");
+const ulc = document.querySelector(".ulc")
+
+btna.addEventListener('click', ()=>{
+    let res = document.querySelector("input").value;
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    li.innerHTML = res;
+    const btn1 = document.createElement("button")
+    btn1.innerHTML = "Completed"
+    const btn2 = document.createElement("button")
+    btn2.innerHTML = "Delete"
+    div.classList.add(`divin`)
+    div.setAttribute('id','divid')
+    btn1.classList.add(`btnd`)
+    btn2.classList.add(`btnd`)
+    div.appendChild(li);
+    div.appendChild(btn1);
+    div.appendChild(btn2);
+    ulc.appendChild(div);
+    btn2.addEventListener('click',()=>{
+        ulc.remove(div);
+    })
+    btn1.addEventListener('click',()=>{
+        document.getElementById('divid').style.opacity = 0.3;
+    })
+});
+
+btnp.addEventListener('click',()=>{
+    let res = document.querySelector("input").value;
+    const div = document.createElement("div");
+    const li = document.createElement("li");
+    li.innerHTML = res;
+    const btn1 = document.createElement("button")
+    btn1.innerHTML = "Completed"
+    let btn2 = document.createElement("button")
+    btn2.innerHTML = "Delete"
+    div.classList.add(`divin`)
+    div.setAttribute('id','divid')
+    btn1.classList.add(`btnd`)
+    btn2.classList.add(`btnd`)
+    div.prepend(btn2);
+    div.prepend(btn1);
+    div.prepend(li);
+    ulc.prepend(div);
+    btn2.addEventListener('click',()=>{
+        ulc.remove(div);
+    })
+    btn1.addEventListener('click',()=>{
+        document.getElementById('divid').style.opacity = 0.3
+    })
+});
+
+btnd.addEventListener("click",()=>{
+    ulc.remove();
+});
+ */
+
+
+/* let nodelist = document.getElementsByTagName("LI")
+let i;
+for(i=0;i<nodelist.length;i++){
+    let btn2 = document.createElement("button")
+    btn2.innerHTML = "Delete"
+    btn2.className="Delete";
+    nodelist[i].appendChild(btn2);
+}*/
+window.onload = (event) => {
+
+    fetch("http://localhost:3000/get",{
+        method:"GET"
+    }).then((resp)=>{
+        resp.json().then((data)=>{
+            for(let i=0;i<data.todos.length;i++){
+                let li = document.createElement("li")
+                li.className = "Li"
+                let t = document.createTextNode(data.todos[i].title);
+                li.appendChild(t);
+                document.getElementById("ul").appendChild(li)
+                let span = document.createElement("SPAN")
+        let btn2 = document.createElement("button")
+        btn2.innerHTML = "Delete"
+        btn2.className = "Delete";
+        span.appendChild(btn2);
+        li.appendChild(span)
+
+        let btn1 = document.createElement("button")
+        btn1.innerHTML = "Completed"
+        btn1.className = "Completed";
+        span.appendChild(btn1);
+        li.appendChild(span)
+
+        btn2.onclick = function () {
+            let div = li.innerHTML
+            const index = div.indexOf("<")
+            const word = div.substring(0,index)
+            //div.style.display = "none"
+            //let classLi = document.getElementsByClassName('li')
+            //console.log(word)
+
+            fetch("http://localhost:3000/deleteId",{
+                method:"DELETE",
+                body:JSON.stringify({
+                    title:word
+                }),
+                headers:{
+                    "Content-Type":"application/json"
+                }
+            }).then((resp)=>{
+                resp.json().then((data)=>{
+                    console.log(data)
+                })
+            })
+            location.reload();
+        }
+
+        btn1.onclick = function () {
+            let div = li
+            div.style.opacity = 0.3
+        }
+            }
+        })
+    })
+    
+    
+    //console.log("page is fully loaded");
+};
+
+function newTask() {
+        let inputValue = document.getElementById("name").value;
+        fetch("http://localhost:3000/add",{
+            method:"POST",
+            body:JSON.stringify({
+                title:inputValue
+            }),
+            headers:{
+                "Content-Type":"application/json"
+            }
+        }).then((resp)=>{
+            resp.json().then((data)=>{
+                //console.log(data)
+            })
+        })
+        location.reload();
+        let li = document.createElement("li")
+        li.className = "Li"
+        let t = document.createTextNode(inputValue);
+        li.appendChild(t);
+        if (inputValue === " ") {
+            alert("Please write something");
+        } else {
+            document.getElementById("ul").appendChild(li)
+        }
+        document.getElementById("name").value = ""
+
+        let span = document.createElement("SPAN")
+        let btn2 = document.createElement("button")
+        btn2.innerHTML = "Delete"
+        btn2.className = "Delete";
+        span.appendChild(btn2);
+        li.appendChild(span)
+
+        let btn1 = document.createElement("button")
+        btn1.innerHTML = "Completed"
+        btn1.className = "Completed";
+        span.appendChild(btn1);
+        li.appendChild(span)
+
+        btn2.onclick = function () {
+            let div = li.innerHTML
+            const index = div.indexOf("<")
+            const word = div.substring(0,index)
+            //div.style.display = "none"
+            //let classLi = document.getElementsByClassName('li')
+            //console.log(word)
+
+            fetch("http://localhost:3000/deleteId",{
+                method:"DELETE",
+                body:JSON.stringify({
+                    title:word
+                }),
+                headers:{
+                    "Content-Type":"application/json"
+                }
+            }).then((resp)=>{
+                resp.json().then((data)=>{
+                    console.log(data)
+                })
+            })
+            location.reload();
+        }
+
+        btn1.onclick = function () {
+            let div = li
+            div.style.opacity = 0.3
+        }
+    }
+
+    function Delete() {
+        fetch("http://localhost:3000/deleteAll",{
+            method:"DELETE"
+        }).then((resp)=>{
+            resp.json().then((data)=>{
+                //console.log(data)
+            })
+        })
+        location.reload()
+        //let li = document.getElementById("ul")
+        //li.remove()
+    }
+    function Priority() {
+        let inputValuep = document.getElementById("name").value;
+        fetch("http://localhost:3000/add",{
+            method:"POST",
+            body:JSON.stringify({
+                title:inputValuep
+            }),
+            headers:{
+                "Content-Type":"application/json"
+            }
+        }).then((resp)=>{
+            resp.json().then((data)=>{
+                //console.log(data)
+            })
+        })
+        let li = document.createElement("li")
+        let inputValue = document.getElementById("name").value;
+        let t = document.createTextNode(inputValue);
+        li.appendChild(t);
+        if (inputValue === " ") {
+            alert("Please write something");
+        } else {
+            document.getElementById("ul").prepend(li)
+        }
+        document.getElementById("name").value = ""
+
+        let span = document.createElement("SPAN")
+        let btn2 = document.createElement("button")
+        btn2.innerHTML = "Delete"
+        btn2.className = "Delete";
+        span.appendChild(btn2);
+        li.appendChild(span)
+
+        let btn1 = document.createElement("button")
+        btn1.innerHTML = "Completed"
+        btn1.className = "Completed";
+        span.appendChild(btn1);
+        li.appendChild(span)
+
+        btn2.onclick = function () {
+            let div = li.innerHTML
+            const index = div.indexOf("<")
+            const word = div.substring(0,index)
+            //div.style.display = "none"
+            //let classLi = document.getElementsByClassName('li')
+            //console.log(word)
+
+            fetch("http://localhost:3000/deleteId",{
+                method:"DELETE",
+                body:JSON.stringify({
+                    title:word
+                }),
+                headers:{
+                    "Content-Type":"application/json"
+                }
+            }).then((resp)=>{
+                resp.json().then((data)=>{
+                    console.log(data)
+                })
+            })
+            location.reload();
+        }
+
+        btn1.onclick = function () {
+            let div = li
+            div.style.opacity = 0.3
+        }
+    }
